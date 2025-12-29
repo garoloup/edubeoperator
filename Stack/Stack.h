35268e9a5388ea28362fd0e52021c54efdeb063b
garoloup <fredwolff@free.fr>
2025-12-29 18:00:47 +0100

    Add std exceptions

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,11 +1,12 @@
 #pragma once
+#include <stdexcept>
 
 class Stack {
     private:
     int stackstore[100];
     int SP;
     public:
-        Stack(void) { SP = 0; }
+        Stack(void) noexcept { SP = 0; }
     void push(int value);
     int pop(void);
     Stack& operator<<(int value);
